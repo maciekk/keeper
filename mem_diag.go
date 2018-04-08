ad9ccad1b8b956c4d21c64b512b6c63f98cdf075
Maciej Kalisiak <mkalisiak@gmail.com>
2018-04-08 01:58:42 -0400

    Update stale reference to pretty.go library.

--- mem_diag.go
+++ mem_diag.go
@@ -6,19 +6,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/maciekk/keeper/pretty"
 	"runtime"
 )
 
 func SprintlnInterestingMemStats(mem *runtime.MemStats) string {
 	return fmt.Sprintln(
-		"alloc", HumanReadable(mem.Alloc, "B"),
-		"totall", HumanReadable(mem.TotalAlloc, "B"),
-		"sys", HumanReadable(mem.Sys, "B"),
-		"halloc", HumanReadable(mem.HeapAlloc, "B"),
-		"hsys", HumanReadable(mem.HeapSys, "B"),
-		"huse", HumanReadable(mem.HeapInuse, "B"),
-		"suse", HumanReadable(mem.StackInuse, "B"),
-		"ssys", HumanReadable(mem.StackSys, "B"),
-		"nextgc", HumanReadable(mem.NextGC, ""))
+		"alloc", pretty.HumanReadable(mem.Alloc, "B"),
+		"totall", pretty.HumanReadable(mem.TotalAlloc, "B"),
+		"sys", pretty.HumanReadable(mem.Sys, "B"),
+		"halloc", pretty.HumanReadable(mem.HeapAlloc, "B"),
+		"hsys", pretty.HumanReadable(mem.HeapSys, "B"),
+		"huse", pretty.HumanReadable(mem.HeapInuse, "B"),
+		"suse", pretty.HumanReadable(mem.StackInuse, "B"),
+		"ssys", pretty.HumanReadable(mem.StackSys, "B"),
+		"nextgc", pretty.HumanReadable(mem.NextGC, ""))
 }
-
